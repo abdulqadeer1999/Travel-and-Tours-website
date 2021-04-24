05ae5f440de4acb7669662ce4c735ee1fca52daa
abdulqadeer1999 <abdulqadeersolangi007@gmail.com>
2021-04-24 16:56:54 +0500

    tour website

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./components/Navbar"
+import {BrowserRouter as Router,Switch,Route } from "react-router-dom"
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} /> 
+      </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import "../../src/App.css"
+import Cards from "../components/Cards"
+import HeroSection from "../components/HeroSection"
+
+
+function Home   ()  {
+        return (
+            <div>
+                <HeroSection />
+                <Cards />
+            </div>
+        )
+}
+
+export default Home;
